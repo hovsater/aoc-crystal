5dda34d812a2708d3746f9a616b2e6b966d19b39
Kevin Hovsäter <kevin@hovsater.com>
2022-12-02 08:33:44 +0100

    Improve day 2, 2022
    Simplify away tuple indexing

--- 2022/day02.cr
+++ 2022/day02.cr
@@ -8,5 +8,5 @@ hands = {
 }
 
 input = File.read("#{__DIR__}/day02.txt").split("\n").map { |r| r.split.map { |h| hands[h] } }
-puts input.sum { |(a, b)| {3, 0, 6}[(a - b) % 3] + b + 1 }
-puts input.sum { |(a, b)| {0, 3, 6}[b] + (a + {-1, 0, 1}[b]) % 3 + 1 }
+puts input.sum { |(a, b)| (b - a + 1) % 3 * 3 + b + 1 }
+puts input.sum { |(a, b)| b * 3 + (a + b - 1) % 3 + 1 }
