04111b6015b23dfb9a0bfc161b9e67f525ff0587
Kevin Hovsäter <kevin@hovsater.com>
2022-12-02 07:40:27 +0100

    Improve day 2, 2022
    Use modulo arithmetic to reduce boilerplate code.

--- 2022/day02.cr
+++ 2022/day02.cr
@@ -1,33 +1,12 @@
-input = File.read("#{__DIR__}/day02.txt")
-
 hands = {
-  "A" => 1,
-  "B" => 2,
-  "C" => 3,
-  "X" => 1,
-  "Y" => 2,
-  "Z" => 3,
+  "A" => 0,
+  "B" => 1,
+  "C" => 2,
+  "X" => 0,
+  "Y" => 1,
+  "Z" => 2,
 }
 
-w = {1 => 3, 2 => 1, 3 => 2}
-l = w.invert
-
-rounds = input.split("\n").map { |r| r.split(" ").map { |h| hands[h] } }
-
-puts rounds.reduce(0) { |acc, (a, b)|
-  if a == b
-    acc + 3 + b
-  elsif w[b] == a
-    acc + 6 + b
-  else
-    acc + b
-  end
-}
-
-puts rounds.reduce(0) { |acc, (a, b)|
-  case b
-  when 1 then acc + w[a]
-  when 2 then acc + 3 + a
-  else        acc + 6 + l[a]
-  end
-}
+input = File.read("#{__DIR__}/day02.txt").split("\n").map { |r| r.split.map { |h| hands[h] } }
+puts input.sum { |(a, b)| {3, 0, 6}[(a - b) % 3] + b + 1 }
+puts input.sum { |(a, b)| {0, 3, 6}[b] + (a + {-1, 0, 1}[b]) % 3 + 1 }
