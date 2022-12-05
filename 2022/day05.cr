6f4a881141386e37c30b1ae3a9a4998d2e66c73c
Kevin Hovsäter <kevin@hovsater.com>
2022-12-05 17:45:28 +0100

    Improve day 5, 2022

--- 2022/day05.cr
+++ 2022/day05.cr
@@ -1,29 +1,12 @@
-stack_layout, move_lines = File.read("#{__DIR__}/day05.txt").split("\n\n")
-*crate_distribution, stack_numbers = stack_layout.lines
+stack_layout, move_instructions = File.read("#{__DIR__}/day05.txt").split("\n\n")
+stacks = stack_layout.lines[..-2].map { |line| (1..line.size).step(4).map { |i| line[i] }.to_a }.transpose.map(&.select(&.letter?))
+moves = move_instructions.lines.map(&.scan(/\d+/).map(&.[0].to_i))
 
-stack_columns = stack_numbers
-  .each_char
-  .with_index
-  .select(&.[0].number?)
-  .map(&.[1])
-  .to_a
-stacks = crate_distribution
-  .map { |cd| stack_columns.map { |c| cd[c] } }
-  .transpose.map { |s| s.reject(&.whitespace?) }
-moves = move_lines
-  .split("\n")
-  .map(&.scan(/\d+/).map(&.[0].to_i))
-
-# Part 1
 puts moves.each_with_object(stacks.clone) { |(n, from, to), stacks|
-  stacks[from - 1].shift(n).each do |crate|
-    stacks[to - 1].unshift(crate)
-  end
+  stacks[from - 1].shift(n).each { |crate| stacks[to - 1].unshift(crate) }
 }.map(&.first).join
 
 # Part 2
 puts moves.each_with_object(stacks.clone) { |(n, from, to), stacks|
-  stacks[from - 1].shift(n).reverse.each do |crate|
-    stacks[to - 1].unshift(crate)
-  end
+  stacks[from - 1].shift(n).reverse.each { |crate| stacks[to - 1].unshift(crate) }
 }.map(&.first).join
