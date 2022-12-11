3697f75cc1be1d27a8baee6a54ae228744b3712c
Kevin Hovsäter <kevin@hovsater.com>
2022-12-11 13:49:25 +0100

    Improve day 8, 2022
    Only check directions once for each cell.

--- 2022/day08.cr
+++ 2022/day08.cr
@@ -1,36 +1,33 @@
 M = File.read_lines("#{__DIR__}/day08.txt").map(&.chars.map(&.to_i))
 
-def visible_dir?(row, col, delta_row, delta_col)
+def in_direction(row, col, delta_row, delta_col)
   n = M[row][col]
 
+  distance = 0
+  visible = true
   while row > 0 && row < M.size - 1 && col > 0 && col < M[0].size - 1
     row += delta_row
     col += delta_col
-    return false if M[row][col] >= n
+    distance += 1
+    if M[row][col] >= n
+      visible = false
+      break
+    end
   end
 
-  true
-end
-
-def score_dir(row, col, delta_row, delta_col)
-  n = M[row][col]
-  p = 0
-
-  while row > 0 && row < M.size - 1 && col > 0 && col < M[0].size - 1
-    row += delta_row
-    col += delta_col
-    p += 1
-    break if M[row][col] >= n
-  end
-  p
+  {visible, distance}
 end
 
 visible = M.size * 2 + (M[0].size - 2) * 2
 scenic_score = 0
 1.upto(M.size - 2).each do |row|
   1.upto(M[row].size - 2).each do |col|
-    visible += 1 if visible_dir?(row, col, -1, 0) || visible_dir?(row, col, 1, 0) || visible_dir?(row, col, 0, -1) || visible_dir?(row, col, 0, 1)
-    score = score_dir(row, col, -1, 0) * score_dir(row, col, 1, 0) * score_dir(row, col, 0, -1) * score_dir(row, col, 0, 1)
+    uv, ud = in_direction(row, col, -1, 0)
+    dv, dd = in_direction(row, col, +1, 0)
+    lv, ld = in_direction(row, col, 0, -1)
+    rv, rd = in_direction(row, col, 0, +1)
+    visible += 1 if {uv, dv, lv, rv}.any?
+    score = ud * dd * ld * rd
     scenic_score = score if score > scenic_score
   end
 end
